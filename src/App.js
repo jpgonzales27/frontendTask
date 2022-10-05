d2e8d0a8030028ad36cc473d41bfe751da37aba4
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-05 18:26:14 -0400

    creando componentes y usando react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TaskList } from "./componentes/TaskList";
+import { TaskForm } from "./componentes/TaskForm";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/tasks/new" element={<TaskForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
