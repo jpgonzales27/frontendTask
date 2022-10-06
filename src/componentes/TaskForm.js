a69c5caf0a72beb7adb1812064b3882e6fca028e
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-05 21:33:01 -0400

    creado formulario para envar la info de la tarea al backed

--- src/componentes/TaskForm.js
+++ src/componentes/TaskForm.js
@@ -1,3 +1,69 @@
+import {
+  Grid,
+  Card,
+  Typography,
+  CardContent,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
 export const TaskForm = () => {
-  return <div>taskForm</div>;
+  const [tarea, setTarea] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(tarea);
+  };
+
+  const handleChange = (e) => {
+    // console.log(e.target.name, e.target.value);
+    setTarea({
+      ...tarea,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={3}>
+        <Card sx={{ mt: 5 }} style={{ padding: "1rem" }}>
+          <Typography variant="5" textAlign="center">
+            Crear Tarea
+          </Typography>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                name="title"
+                variant="filled"
+                label="Titulo"
+                sx={{ display: "block", margin: ".5rem 0" }}
+                onChange={handleChange}
+              />
+              <TextField
+                name="description"
+                variant="filled"
+                label="Description"
+                multiline
+                rows={4}
+                sx={{ display: "block", margin: ".5rem 0" }}
+                onChange={handleChange}
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Guardar
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
