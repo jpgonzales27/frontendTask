967f564b33dbbc7e6a93aa68eea5422acce72b4b
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-06 19:44:55 -0400

    crear tarea desde react

--- src/componentes/TaskForm.js
+++ src/componentes/TaskForm.js
@@ -5,8 +5,10 @@ import {
   CardContent,
   TextField,
   Button,
+  CircularProgress,
 } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const TaskForm = () => {
   const [tarea, setTarea] = useState({
@@ -14,9 +16,31 @@ export const TaskForm = () => {
     description: "",
   });
 
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(tarea);
+
+    setLoading(true);
+
+    //Usando fetch enviamos nuestra tarea al endpoint deseado
+    const res = await fetch("http://localhost:4000/tasks", {
+      //definimos el metodo de la peticion
+      method: "POST",
+      //debemos converitir nuestro objeto a string
+      body: JSON.stringify(tarea),
+      //definimos el formato para que la app entienda que le enviamos un JSON
+      headers: { "Content-Type": "application/json" },
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    setLoading(false);
+    navigate("/");
   };
 
   const handleChange = (e) => {
@@ -57,8 +81,17 @@ export const TaskForm = () => {
                 sx={{ display: "block", margin: ".5rem 0" }}
                 onChange={handleChange}
               />
-              <Button variant="contained" color="primary" type="submit">
-                Guardar
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                disabled={!tarea.title || !tarea.description}
+              >
+                {loading ? (
+                  <CircularProgress color="inherit" size={25} />
+                ) : (
+                  "Save"
+                )}
               </Button>
             </form>
           </CardContent>
