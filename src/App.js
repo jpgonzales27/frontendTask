c8dc053481c919012c6569c2b0570604c794ee93
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-05 20:48:37 -0400

    instalando material ui, roboto font y material icons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { TaskList } from "./componentes/TaskList";
 import { TaskForm } from "./componentes/TaskForm";
+import { Navbar } from "./componentes/Navbar";
+import { Container } from "@mui/material";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/tasks/new" element={<TaskForm />} />
-      </Routes>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/tasks/new" element={<TaskForm />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
--- src/componentes/Navbar.js
+++ src/componentes/Navbar.js
@@ -1,3 +1,14 @@
+import { Button } from "@mui/material";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      Navbar
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color="secondary">
+        Contained
+      </Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
 };
